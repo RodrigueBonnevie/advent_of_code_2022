298ac50e1dd6720dbc2d2d341b978224ae4154e7
RodrigueBonnevie <bonnevie@kth.se>
2023-01-08 12:53:44 +0100

    day2 part two

--- day2/day2.py
+++ day2/day2.py
@@ -2,55 +2,83 @@ f = open("input_day2.txt","r")
 
 def pointsForChoice(choice):
     match choice:
-        case "X":
+        case "A":
             return 1
-        case "Y":
+        case "B":
             return 2
-        case "Z":
+        case "C":
             return 3
         case _:
             return 0 
 
-def pointsForResult(elfsChoise, myChoice):
-    match elfsChoise:
+def pointsForResult(elfsChoice, myChoice):
+    match elfsChoice:
         case "A":
             match myChoice:
-                case "X":
+                case "A":
                     return 3
-                case "Y":
+                case "B":
                     return 6
-                case "Z":
+                case "C":
                     return 0
                 case _:
                     return 0 
         case "B":
             match myChoice:
-                case "X":
+                case "A":
                     return 0
-                case "Y":
+                case "B":
                     return 3
-                case "Z":
+                case "C":
                     return 6
                 case _:
                     return 0 
         case "C":
             match myChoice:
-                case "X":
+                case "A":
                     return 6
-                case "Y":
+                case "B":
                     return 0
-                case "Z":
+                case "C":
                     return 3
                 case _:
                     return 0 
         case _:
             return 0 
 
+def choiceFromOutcome(elfsChoice, outcome):
+    match outcome:
+        case "X": # Lose
+            match elfsChoice:
+                case "A":
+                    return "C"
+                case "B":
+                    return "A"
+                case "C":
+                    return "B"
+                case _:
+                    return "" 
+        case "Y": # Draw
+            return elfsChoice
+        case "Z": # Win
+            match elfsChoice:
+                case "A":
+                    return "B"
+                case "B":
+                    return "C"
+                case "C":
+                    return "A"
+                case _:
+                    return "" 
+        case _:
+            return "" 
+
 
 rounds = f.readlines()
 totalPoints = 0
 for round in rounds:
-    totalPoints += pointsForChoice(round[2])
-    totalPoints += pointsForResult(round[0], round[2])
+    myChoice = choiceFromOutcome(round[0],round[2])
+    totalPoints += pointsForChoice(myChoice)
+    totalPoints += pointsForResult(round[0], myChoice)
 
 print(f"Total points for strategy {totalPoints}")

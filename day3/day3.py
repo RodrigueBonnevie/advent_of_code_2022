40bdd2725159ead8dd5cdf290ba51046b298bcb7
RodrigueBonnevie <bonnevie@kth.se>
2023-01-10 00:26:39 +0100

    day 3 part two

--- day3/day3.py
+++ day3/day3.py
@@ -4,9 +4,9 @@ def splitBackpackIntoCompartment(backpack):
     return compartments
 
 
-def findCommonLetterInStrings(string1, string2):
-    for letter in string1:
-        letterPosition = string2.find(letter)
+def findCommonLetterInCompartments(compartment1, compartment2):
+    for letter in compartment1:
+        letterPosition = compartment2.find(letter)
         if letterPosition != -1:
             return letter
     return ""
@@ -20,17 +20,34 @@ def calculateItemPriority(item):
         return asciiNumber - 96
 
 
+def findCommonLetterInBackpacks(backpack1, backpack2, backpack3):
+    for letter in backpack1:
+        letterPosition2 = backpack2.find(letter)
+        if letterPosition2 != -1:
+            letterPosition3 = backpack3.find(letter)
+            if letterPosition3 != -1:
+                return letter
+    return ""
+
+
 def main():
     f = open("input_day3.txt", "r")
     backpacks = f.readlines()
 
-    prioritySum = 0
+    itemSum = 0
     for backpack in backpacks:
         compartments = splitBackpackIntoCompartment(backpack)
-        item = findCommonLetterInStrings(compartments[0], compartments[1])
-        prioritySum += calculateItemPriority(item)
+        item = findCommonLetterInCompartments(compartments[0], compartments[1])
+        itemSum += calculateItemPriority(item)
 
-    print(prioritySum)
+    badgeSum = 0
+    for i in range(0, len(backpacks)-2, 3):
+        badge = findCommonLetterInBackpacks(
+            backpacks[i], backpacks[i+1], backpacks[i+2])
+        badgeSum += calculateItemPriority(badge)
+
+    print(f"Priority sum of items {itemSum}")
+    print(f"Priority sum of badges {badgeSum}")
 
 
 if __name__ == "__main__":
